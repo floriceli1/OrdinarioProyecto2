f76381b98f8315a36c30e28bd4b2f1fc5b1e90ff
floriceli1 <gonzalezruizfloriceli183@gmail.com>
2023-02-07 12:11:32 -0600

    Eliminar usuario

--- OrdinarioProyecto/Controllers/UsuariosController.cs
+++ OrdinarioProyecto/Controllers/UsuariosController.cs
@@ -81,7 +81,7 @@ namespace OrdinarioProyecto.Controllers
             }
             return View(usuario);
         }
-
+        //Accion que nos ayuda a editar nuestro usuario
         // POST: Usuarios/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -116,7 +116,7 @@ namespace OrdinarioProyecto.Controllers
             }
             return View(usuario);
         }
-
+        //Accion que nos permite eliminar nuestro usuario
         // GET: Usuarios/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
