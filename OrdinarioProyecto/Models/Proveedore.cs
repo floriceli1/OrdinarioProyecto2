df12c05ba80e9fef38661e59ac6d851d0c755d1f
floriceli1 <gonzalezruizfloriceli183@gmail.com>
2023-02-07 12:18:10 -0600

    Entity proveedores

--- OrdinarioProyecto/Models/Proveedore.cs
+++ OrdinarioProyecto/Models/Proveedore.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 
 namespace OrdinarioProyecto.Models;
-
+//creacion de la clase proveedor
 public partial class Proveedore
-{
+{    
+    //Definicion de las variables que ocupa nuetra tabla proveedores
     public int Id { get; set; }
 
     public string? Nombre { get; set; }
